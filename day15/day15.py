7d585673a39193d2bf1daaee137774fbb5832f6e
Bogdan Tudorache <bogdanmihait10@gmail.com>
2023-09-23 18:42:51 +0300

    add day15 part 2

--- day15/day15.py
+++ day15/day15.py
@@ -30,9 +30,9 @@ def day15_task1():
 
     print(num_positions)
 
-def day15_task2():
+def day15_task2(search_range, file_name):
     pairs = []
-    with open("input.txt", "r") as f:
+    with open(file_name, "r") as f:
         for line in f.readlines():
             stripped = line.strip()
             first, second = stripped.split(": ")
@@ -44,25 +44,37 @@ def day15_task2():
             beacon_x_int, beacon_y_int = int(beacon_x.split("=")[1]), int(beacon_y.split("=")[1])
             pairs.append([(sensor_x_int, sensor_y_int), (beacon_x_int, beacon_y_int)])
 
-    search_range = 20
-    positions = [[0 for _ in range(search_range)] for _ in range(search_range)]
-    num_positions = 0
+    # curr_pos = [0 for _ in range(search_range)]
+    found_pair = False
+    for i in range(search_range):
+        if found_pair:
+            break
 
-    for sensor_coords, beacon_coords in pairs:
-        sensor_x, sensor_y = sensor_coords
-        beacon_x, beacon_y = beacon_coords
-        manhattan_dist = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
+        ranges = []
+        for sensor_coords, beacon_coords in pairs:
+            sensor_x, sensor_y = sensor_coords
+            beacon_x, beacon_y = beacon_coords
+            manhattan_dist = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
 
-        if abs(sensor_y - target_y) <= manhattan_dist:
-            diff = abs(manhattan_dist - abs(target_y - sensor_y))
-            for i in range(sensor_x - diff, sensor_x + diff):
-                if positions[i] == 0:
-                    positions[i] = 1
-                    num_positions += 1
+            if abs(sensor_y - i) <= manhattan_dist:
+                diff = abs(manhattan_dist - abs(i - sensor_y))
+                ranges.append((max(sensor_x - diff, 0), min(sensor_x + diff, search_range)))
+        
+        # overlap ranges
+        ranges.sort(key=lambda pair: pair[0])
 
-    print(num_positions)
+        _, biggest_end = ranges[0]
+        for j in range(1, len(ranges)):
+            curr_start, curr_end = ranges[j]
+            if curr_start - biggest_end == 2:
+                print(i, (biggest_end + 1), (biggest_end + 1) * 4000000 + i)
+                found_pair = True
+                break
+            _ = curr_start
+            biggest_end = max(biggest_end, curr_end)
+        
 
 
 if __name__ == '__main__':
-    day15_task1()
-    day15_task2()
+    # day15_task1()
+    day15_task2(4000000, 'input.txt')
